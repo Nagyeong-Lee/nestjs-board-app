d08edd39c91a7a4cdcd0262bceb1624110ba0935
ng-lee <nhd5607@gmail.com>
2025-07-01 02:28:55 +0900

    feature: board entity/repository 추가

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
 
 @Injectable()
 export class BoardService {
+  constructor(private boardRepository: BoardRepository) {}
+
   test(): string {
     return 'hello world';
   }
