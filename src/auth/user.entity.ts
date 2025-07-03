f722519004df6e89f81cbde54a5b8396889c85a0
ng-lee <nhd5607@gmail.com>
2025-07-04 01:47:26 +0900

    feature: user 엔티티 추가

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @CreateDateColumn({name: 'created_at'})
+  createdAt: Timestamp;
+
+  @UpdateDateColumn({name: 'modified_at'})
+  modifiedAt: Timestamp;
+}
