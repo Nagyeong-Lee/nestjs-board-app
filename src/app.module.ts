3265ae83cf39387966bb26119ed6706924d0c0ac
ng-lee <nhd5607@gmail.com>
2025-07-03 23:59:52 +0900

    feature: auth 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { BoardModule } from './board/board.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeORMConfig } from './configs/typeORMConfig';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeORMConfig),
-    BoardModule
+    BoardModule,
+    AuthModule
   ]
 })
 export class AppModule {}
