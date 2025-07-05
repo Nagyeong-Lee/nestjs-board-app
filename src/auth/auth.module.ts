dc7d450e4d1518feecee14f40a2d1c6d5d4d9d29
ng-lee <nhd5607@gmail.com>
2025-07-06 01:23:21 +0900

    fix: auth 모듈 > jwt, passport 모듈 임포트

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeormModule } from '../common/typeorm.module';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeormModule.forCustomRepository([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secret1234',
+      signOptions: { expiresIn: 60 * 60 },
+    }),
+    TypeormModule.forCustomRepository([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
