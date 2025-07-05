76f323f6289f766f47875955ff58eea5a635a9ad
ng-lee <nhd5607@gmail.com>
2025-07-06 01:27:06 +0900

    fix: 로그인 성공 시 access token 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
   }
 
   @Post('/signin')
-  signIn(@Body() authCredentialDto: AuthCredentialDto): Promise<string> {
+  signIn(@Body() authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,20 +3,26 @@ import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private authRepository: UserRepository) {}
+  constructor(
+    private authRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async createUser(authCredentialDto: AuthCredentialDto): Promise<User> {
     return this.authRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(authCredentialDto: AuthCredentialDto,): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.authRepository.findOne({ where: { username } });
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      const payload = { username };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
     }
     throw new UnauthorizedException('login fail');
   }
