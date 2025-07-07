36893d723927bd1032e050cb4ef79777fab10513
ng-lee <nhd5607@gmail.com>
2025-07-07 22:48:38 +0900

    fix: board entity 컬럼 속성 및 user와 연관관계 매핑

--- src/board/board.entity.ts
+++ src/board/board.entity.ts
@@ -1,18 +1,38 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  UpdateDateColumn,
+} from 'typeorm';
 import { BoardStatus } from './board-status.enum';
+import { User } from '../auth/user.entity';
 
 @Entity()
 export class Board extends BaseEntity {
-
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 50, nullable: false })
   title: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 200, nullable: false })
   desc: string;
 
-  @Column()
+  @Column({ type: 'varchar', enum: BoardStatus })
   status: BoardStatus;
+
+  @ManyToOne(() => User, { eager: false })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Timestamp;
+
+  @UpdateDateColumn({ name: 'modified_at' })
+  modifiedAt: Timestamp;
 }
